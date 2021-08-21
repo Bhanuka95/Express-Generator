03a43f7e8b52dd57b4d6e29c55608e0063418c91
Bhanuka Don <donbhanuka3@gmail.com>
2021-08-21 11:18:01 +0530

    passport JWT authenticatin

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 var session = require('express-session')
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
-var authenticate = require('./authenticate')
+var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,8 +18,10 @@ var leaderRouter = require('./routes/leaderRouter');
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+  useMongoClient: true
+});
 
 connect.then((db) =>{
   console.log('Connected correctly to the server');
@@ -35,36 +38,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-//setting session middleware
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 //Incoming user should be able to access the index file and the users file without being authenticated
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-//AUTHORIZATION MIDDLEWARE
-function auth(req, res, next){
-  if(!req.user){
-      var err = new Error('You are not authenticated!');       
-      err.status = 403;
-      next(err);
-      return;
-  }
-  else{
-    next();
-  }
-}
-app.use(auth); //before client can access under resources, he has to first be authorized
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- authenticate.js
+++ authenticate.js
@@ -1,9 +1,44 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+var config = require('./config');
+
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 
 //since we are still using sessions to track user
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+//create token and give it to us
+exports.getToken = function(user){
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+};
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
+
+//when passport passes the request message, it will use the strategy, extract information and load
+//it on to our request message
+exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) =>{
+    console.log("JWT payload: ", jwt_payload);
+    User.findOne({_id: jwt_payload._id}, (err, user) =>{
+        if(err) {
+            return done(err, false);
+        }
+        else if(user){
+            return done(null, user);
+        }
+        else{
+            return done(null, false);
+        }
+    });
+}));
+
+exports.verifyUser = passport.authenticate('jwt', {session: false});
+
